6dc78cbd1ceea60dec1020007c526bd08cd50b9f
Tim Yang <timyang@mit.edu>
2013-04-09 16:02:51 -0400

    Implement freestyle page. Add logo. Add tooltips.

--- js/freestyle.js
+++ js/freestyle.js
@@ -3,16 +3,15 @@ $(document).ready(function() {
 	// Initialize Bootstrap-Select
 	$(".selectpicker").selectpicker();
 
-	// Initialize tooltips.
-	$("a[rel='tooltip']").tooltip()
 
-	// Used to keep track of the opponent's text.
-	var opponentText = "foo";
+	$("a[rel='tooltip']").tooltip()
 
 	// When "Start" is pressed, get the desired topic and time. Then, run a 3 second countdown. Then,
 	// populate the Topic and Time and enable the input text. When the time is up, get and display the
 	// results.
 	$("#start").click(function() {
+
+
 		var selectCategory = $("#select-category").val();
 		var selectTime = $("#select-time").val();
 
@@ -28,19 +27,6 @@ $(document).ready(function() {
 			$("#info").html("");
 			$("#info").html("<div width='100%' style='font-size:2em;font-weight:bold'><span style='text-align:left'>Topic: "+topic+"</span><span style='float:right; position:relative'>Time Remaining: <span id='timer'></span></span></div>");
 			countdownTimer(seconds); // Start the Time Remaining timer.
-
-			setTimeout(function(){
-				if (getResults()) {
-					$("#info").removeClass("alert-info");
-					$("#info").addClass("alert-success");
-					$("#info").html("<div width='100%' style='font-size:2em;font-weight:bold;text-align:center;margin-bottom:15px'>You win!</div><div width='100%' style='font-size:1em;font-weight:bold;text-align:center'><a href='results.html'>View Results</a> | <a href='battle.html'>Play Again</a></div>");
-				} else {
-					$("#info").removeClass("alert-info");
-					$("#info").addClass("alert-error");
-					$("#info").html("<div width='100%' style='font-size:2em;font-weight:bold;text-align:center;margin-bottom:15px'>Sorry, you lose.</div><div width='100%' style='font-size:1em;font-weight:bold;text-align:center'><a href='results.html'>View Results</a> | <a href='battle.html'>Play Again</a></div>");
-				}
-			},seconds*1000+2000); // TODO: This is a hardcoded 2 second wait for results. Update to accommodate variable results return time.
-
 		},4000); // 4 second wait to accomodate the countdown.
 
 	});
@@ -71,22 +57,16 @@ $(document).ready(function() {
 	    	{
 	    		$("#timer").html(toMinutes(count));
 	    		clearInterval(counter);
-	            $("#info").html("<div width='100%' style='font-size:2em;text-align:center;font-weight:bold' >Time's up! Waiting for results...</div>");
-	            $("#you-box").attr("disabled", true);
-
+	    		$("#info").removeClass("alert-info");
+	    		$("#info").addClass("alert-success");
+	    		$("#info").html("<div width='100%' style='font-size:2em;font-weight:bold;text-align:center;margin-bottom:15px'>Time's up!</div><div width='100%' style='font-size:1em;font-weight:bold;text-align:center'><a href='results.html'>View Results</a> | <a href='freestyle.html'>Play Again</a></div>");	            
+	    		$("#you-box").attr("disabled", true);
 	            return;
 	        }
 
 	        $("#timer").html(toMinutes(count));
-	        updateOpponentBox();
 	    }
 	};
-
-	// Gets the results of a writing battle.
-	// TODO: Actually get results.
-	var getResults = function() {
-		return true;
-	}
 
 	// Gets a topic related to the input category.
 	// TODO: Actually implement categories and topics.
@@ -112,22 +92,5 @@ $(document).ready(function() {
 
 		return minutes+":"+seconds;
 	}
-
-	// Update the opponent box's text.
-	var updateOpponentBox = function() {
-		opponentText = getOpponentText();
-		$("#opp-box").html(getOpponentText);
-	}
-
-	// Get the opponent's text. 
-	// TODO: Actually get the opponent's text.
-	var getOpponentText = function() {
-		if (Math.floor(Math.random() * 3) == 1){
-			return opponentText+" "+['foo','bar','baz'][Math.floor(Math.random() * 3)];
-		} else {
-			return opponentText;
-		}
-	}
-
 });
 
