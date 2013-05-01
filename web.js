fc75223da697a47b8a6edbc6d6c03e59b11d2581
tokutei12 <tokutei12@gmail.com>
2013-05-01 13:54:42 -0400

    serving static content on node.js

--- web.js
+++ web.js
@@ -1,12 +1,8 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// compress content using gzip
+app.use(express.compress());
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 3000);
