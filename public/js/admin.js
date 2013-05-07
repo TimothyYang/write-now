1f23e8f8ae3bdc12521f0fbd31da61c5ba14718a
Timothy Yang <timyang@mit.edu>
2013-05-07 17:59:55 -0400

    Add user adding

--- public/js/admin.js
+++ public/js/admin.js
@@ -7,12 +7,16 @@ $(document).ready(function() {
 	})
     });
 
+    // Add a user with the name benB.
     $("#add-user").click(function(){	
-	$.get("/add-user", function(string) {
-//	    $("#accounts-well").text(string)
-	    $("#add-user-well").text("done!")
-	})
+        $.ajax( { url: "https://api.mongolab.com/api/1/databases/heroku_app15381569/collections/accounts?apiKey=CPBZJI2Y7k0K8NAlVZwM9QqHctUJrWUv",
+                  data: JSON.stringify( { "name":"benB" } ),
+                  type: "POST",
+                  contentType: "application/json",
+		  success: function(data, textStatus, jqXHR) { $("#add-user-well").text("Done!"); }
+		})
     });
+
 
 
 });
