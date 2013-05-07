bd8223bad99c24f4deb5837c4c2469497c3a07a9
tokutei12 <tokutei12@gmail.com>
2013-05-07 19:22:24 -0400

    logins complete, current user is visible on every page, can view profile or preferences by clicking name in top right

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	//replace the user's name on the profile page
 	$.get("/get-current-user", function(string) {
 		if(string=="User Not Logged In"){
-			$("#currentuser").text(string.charAt(0).toUpperCase() + string.slice(1));
+			$("#currentuser").text("Guest");
 		} else{
 			$("#currentuser").text(string.charAt(0).toUpperCase() + string.slice(1));
 		}
